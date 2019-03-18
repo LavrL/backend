067c05677f613316034818558d930c2e9a641a3a
Lavrentijs L <llembersk@gmail.com>
2019-03-18 22:43:24 +0200

    removed commented lines

--- src/route/properties.js
+++ src/route/properties.js
@@ -18,10 +18,6 @@ router.get('/', async (ctx) => {
   }
 })
 
-// router.put('/', async (ctx) => {
-//  ctx.throw(501)
-// })
-
 router.get('/:propertyId', async (ctx) => {
   const { user } = ctx.state
   const { propertyId } = ctx.params
@@ -40,9 +36,6 @@ router.get('/:propertyId', async (ctx) => {
   }
 })
 
-// router.post('/:propertyId', async (ctx) => {
-//  ctx.throw(501)
-// })
 // ----------
 router.post('/', body(), async (ctx) => {
     const { address, user_id } = ctx.request.body
