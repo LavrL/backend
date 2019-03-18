833934f77e58b467992d8b7ebf76ca94f3a79d76
Lavrentijs L <llembersk@gmail.com>
2019-03-18 19:24:57 +0200

    router put changes 9

--- src/route/properties.js
+++ src/route/properties.js
@@ -94,7 +94,7 @@ router.put('/:address', body(), async (ctx) => {
     const { address } = ctx.request.body
 
     const property = await PropertyModel.query()
-        .where({ user_id: user.id, address: oldAddress }).update({ address })
+        .update({ address }).where({ user_id: user.id, address: oldAddress })
 
     if (property === undefined) {
         return ctx.throw(404)
