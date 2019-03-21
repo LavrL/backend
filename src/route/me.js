103501393051698ddafbf8f2a41005f054c1e301
Lavrentijs L <llembersk@gmail.com>
2019-03-22 01:20:55 +0200

    12

--- src/route/me.js
+++ src/route/me.js
@@ -26,7 +26,7 @@ router.put('/', body(), async (ctx) => {
     console.log('username - ' + user.username);
 
     const user_ = await CreateUserModel.query()
-        .update({ password }).where({ username: user.username, password: user.password })
+        .update({ username, password }).where({ username: user.username, password: user.password })
 
     if (user_ === undefined) {
         return ctx.throw(404)
